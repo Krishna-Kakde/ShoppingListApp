30aad9e36aa8365b31fcb98e89c7b5655fdb2c15
Krishna-Kakde <krishnakakde2007@gmail.com>
2025-12-15 16:46:21 +0530

    Making shoppingListItem Composable and adding icon button to it

--- app/src/main/java/com/example/shoppinglist/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglist/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.example.shoppinglist
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,8 +10,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +28,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem (
@@ -114,5 +125,30 @@ fun ShoppingListApp (modifier: Modifier = Modifier ) {
                 }
             }
         )
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item : ShoppingItem,
+    OnEditClick: () -> Unit,
+    OnDeleteClick: () -> Unit,
+){
+    Row(modifier = Modifier.fillMaxWidth().padding(8.dp).border(
+        border = BorderStroke(2.dp,Color.Cyan),
+        shape = RoundedCornerShape(20))
+    ) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
+        Text(text = "Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
+        Row(modifier = Modifier.padding(8.dp)) {
+            IconButton(onClick = OnEditClick) {
+                Icon(
+                    imageVector = Icons.Default.Edit,contentDescription = null)
+            }
+            IconButton(onClick = OnDeleteClick) {
+                Icon(
+                    imageVector = Icons.Default.Delete,contentDescription = null)
+            }
+        }
     }
 }
